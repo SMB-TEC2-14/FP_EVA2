884c041fecd28debb11bec3d3cdcc85ec98ff21e
SMB-TEC2-14 <L24550835@chihuahua2.tecnm.mx>
2024-10-21 17:36:35 -0600

    Add files via upload

---
+++ EVA2_15_ARREGLOS/src/eva2_15_arreglos/EVA2_15_ARREGLOS.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_15_arreglos;
+
+/**
+ *
+ * @author santy
+ */
+public class EVA2_15_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //VARIABLES
+        
+        
+        //CODE
+        int[] arreglo = new int [10];
+        //Manejo por indices, el primer elemento de un arreglo es = 0 y el ultimo es el tamaño seleccionado -1 (en este caso seria 10 - 1 = 9)
+        arreglo[0] = 100;
+        arreglo[1] = 200;
+        arreglo[2] = 300;
+        arreglo[3] = 400;
+        arreglo[4] = 500;
+        arreglo[5] = 600;
+        arreglo[6] = 700;
+        arreglo[7] = 800;
+        arreglo[8] = 900;
+        arreglo[9] = 1000;
+        
+        System.out.println(" ");
+        System.out.println("---");
+        System.out.println(arreglo[0]);
+        System.out.println("---");
+        System.out.println(arreglo[1]);
+        System.out.println("---");
+        System.out.println(arreglo[2]);
+        System.out.println("---");
+        System.out.println(arreglo[3]);
+        System.out.println("---");
+        System.out.println(" ");
+        
+    }
+    
+}
